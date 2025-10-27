70d5ac2e5a2ccbb0fc0fb734f3bea90841a173a0
bndnt <67003594+bndnt@users.noreply.github.com>
2025-10-26 20:44:03 -0400

    cafe

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,47 @@
 import { useState } from "react";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import css from "./App.module.css";
-import type Votes from "../../types/votes";
+import type { Votes, VoteType } from "../../types/votes";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 function App() {
-  const [vote, setVote] = useState<Votes>({
+  const [votes, setVote] = useState<Votes>({
     good: 0,
     neutral: 0,
     bad: 0,
   });
+  const handleVote = (type: VoteType) => {
+    setVote((prev) => ({ ...prev, [type]: prev[type] + 1 }));
+  };
+  function resetVotes() {
+    setVote({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  }
+  let totalVotes = votes.good + votes.bad + votes.neutral;
+  let positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 }
--- src/types/votes.ts
+++ src/types/votes.ts
@@ -1,6 +1,7 @@
-export default interface Votes {
+export interface Votes {
   good: number;
   neutral: number;
   bad: number;
 }
-type VoteType = "good" | "neutral" | "bad";
+
+export type VoteType = "good" | "neutral" | "bad";
